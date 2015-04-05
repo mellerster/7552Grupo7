712b6566a95daae47495cc3e4dc906d00dd924a2
mellerster <concdiego@gmail.com>
2015-04-05 17:12:32 -0300

    Cambiado el nombre de la clase de configurar perfil para que respete la convencion de java

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ConfigurarPerfil.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ConfigurarPerfil.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class configurar_perfil extends ActionBarActivity {
+public class ConfigurarPerfil extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
