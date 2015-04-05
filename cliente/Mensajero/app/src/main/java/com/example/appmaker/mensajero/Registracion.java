e2a8652a02b55499969a43908d22dae926063440
mellerster <concdiego@gmail.com>
2015-04-05 17:22:29 -0300

    Cambiado el nombre de Registracion para que respete la convencion de Java

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/Registracion.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/Registracion.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class registracion extends ActionBarActivity {
+public class Registracion extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
