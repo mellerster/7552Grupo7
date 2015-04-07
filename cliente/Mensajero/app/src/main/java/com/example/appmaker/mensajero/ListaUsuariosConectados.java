353c04e8bd02b0d91296719e23983272fb048de6
mellerster <concdiego@gmail.com>
2015-04-07 15:43:08 -0300

    Cambiada la pantalla ListaUsuariosConectados para que cargue la información a partir de un List dinamicamente

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ListaUsuariosConectados.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ListaUsuariosConectados.java
@@ -1,17 +1,55 @@
 package com.example.appmaker.mensajero;
 
+import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class ListaUsuariosConectados extends ActionBarActivity {
+    String tag = "Events"; // Tag para usar Log.d y poder filtrar por este tag
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_usuarios_conectados);
+        List<Usuario> usuarios = obtenerUsuarios();
+        MostrarUsuarios(usuarios);
+    }
+
+    private List<Usuario> obtenerUsuarios() {
+        List<Usuario> usuarios = new LinkedList<Usuario>();
+        usuarios.add(new Usuario("diego", "diego"));
+        usuarios.add(new Usuario("cesar", "cesar"));
+        usuarios.add(new Usuario("ramiro", "ramiro"));
+        usuarios.add(new Usuario("tomas", "tomas"));
+        return usuarios;
+    }
+
+    private void MostrarUsuarios(List<Usuario> usuarios) {
+        LinearLayout gridUsuarios = (LinearLayout) findViewById(R.id.gridUsuarios);
+        for (Usuario usuario : usuarios) {
+            TextView nombre = new TextView(this);
+            nombre.setText(usuario.getNombre());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.FILL_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            nombre.setLayoutParams(params);
+            gridUsuarios.addView(nombre);
+        }
     }
 
 
