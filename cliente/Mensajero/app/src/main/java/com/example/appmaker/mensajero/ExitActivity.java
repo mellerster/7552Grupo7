7d496ce865721bc5c4d9a805d9e23613f6f2d68c
mellerster <concdiego@gmail.com>
2015-04-28 19:08:25 -0300

    Se renombro la activity Main a Exit para pruebas de salir de la app

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ExitActivity.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ExitActivity.java
@@ -1,42 +1,16 @@
 package com.example.appmaker.mensajero;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.util.Log;
 
 public class ExitActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (getIntent().getBooleanExtra("EXIT", false)) {
-            finish();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Log.d("Salio", "Salio?");
+        finish();
     }
 }
