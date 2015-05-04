3ca0c0dae32ea93f26aea25156731b99339d53dc
mellerster <concdiego@gmail.com>
2015-05-04 10:48:10 -0300

    El listado de usuarios conectados parsea lo que envia el server ahora, faltan cambios, esta todo mockeado todavia

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/UsuarioProxy.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/UsuarioProxy.java
@@ -1,5 +1,13 @@
 package com.example.appmaker.mensajero;
 
+import android.util.JsonReader;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,41 +24,124 @@ public class UsuarioProxy {
 
     /**
      * Obtiene el usuario con el token para hablar con el servidor
+     *
      * @return usuario que tiene el token
      */
-    public static Usuario getUsuario(){
+    public static Usuario getUsuario() {
         return usuario;
     }
 
     /**
-       Trae del servidor la lista de usuarios conectados
-        @return Un List tipado de Usuario
+     * Trae del servidor la lista de usuarios conectados
+     *
+     * @return Un List tipado de Usuario
      */
-    public List<Usuario> getUsuariosConectados(){
+    public List<Usuario> getUsuariosConectados() {
         //Mockup de Usuarios
-        List<Usuario> usuarios = new LinkedList<Usuario>();
-        Usuario usuario;
-        usuario = new Usuario("Diego");
-        usuario.conectar();
-        usuarios.add(usuario);
-        usuario = new Usuario("Cesar");
-        usuario.conectar();
-        usuarios.add(usuario);
-        usuario = new Usuario("Ramiro");
-        usuario.conectar();
-        usuarios.add(usuario);
-        usuario = new Usuario("Tomas");
-        usuario.conectar();
-        usuarios.add(usuario);
+        String jsonRecibido = "\n" +
+                "{\n" +
+                "\t\"Status\" : \"OK\",\n" +
+                "\t\"Token\" : 0,\n" +
+                "\t\"Usuarios\" : \n" +
+                "\t[\n" +
+                "\t\t{\n" +
+                "\t\t\t\"Estado\" : \"Testing\",\n" +
+                "\t\t\t\"Nombre\" : \"pepe\",\n" +
+                "\t\t\t\"Status\" : \"OK\",\n" +
+                "\t\t\t\"Token\" : 0,\n" +
+                "\t\t\t\"UltimaActividadFecha\" : \"2015/12/31\",\n" +
+                "\t\t\t\"UltimaActividadHora\" : \"00:00\"\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"Estado\" : \"Testing\",\n" +
+                "\t\t\t\"Nombre\" : \"pepe\",\n" +
+                "\t\t\t\"Status\" : \"OK\",\n" +
+                "\t\t\t\"Token\" : 0,\n" +
+                "\t\t\t\"UltimaActividadFecha\" : \"2015/12/31\",\n" +
+                "\t\t\t\"UltimaActividadHora\" : \"00:00\"\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"Estado\" : \"Testing\",\n" +
+                "\t\t\t\"Nombre\" : \"pepe\",\n" +
+                "\t\t\t\"Status\" : \"OK\",\n" +
+                "\t\t\t\"Token\" : 0,\n" +
+                "\t\t\t\"UltimaActividadFecha\" : \"2015/12/31\",\n" +
+                "\t\t\t\"UltimaActividadHora\" : \"00:00\"\n" +
+                "\t\t}\n" +
+                "\t]\n" +
+                "}";
+        List<Usuario> usuarios = parsearJSon(jsonRecibido);
         return usuarios;
+    }
+
+    private List<Usuario> parsearJSon(String jsonRecibido) {
+        InputStream in = new ByteArrayInputStream(jsonRecibido.getBytes());
+        String token = "";
+        List<Usuario> usuarios = new ArrayList<>();
+        try {
+            JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+            try {
+                reader.beginObject();
+                while (reader.hasNext()) {
+                    String name = reader.nextName();
+                    switch (name) {
+                        case "Status":
+                            String status = reader.nextString();
+                            if (!status.equals("OK")) {
+                                ///TODO: Crear Excepcion: EstadoRecibidoInvalidoException
+                                Log.e("Obtencion Listado Usuarios Conectados", "Status es: " + status);
+                                throw new Exception();
+                            }
+                            break;
+                        case "Token":
+                            token = reader.nextString();
+                            break;
+                        case "Usuarios":
+                            reader.beginArray();
+
+                            while (reader.hasNext()) {
+                                usuarios.add(readUsuarioListado(reader));
+                            }
+                            reader.endArray();
+                            break;
+
+                    }
+                }
+            } finally {
+                reader.close();
+            }
+        } catch (Exception ex) {
+            Log.e("Leer Listado de Usuarios Conectados", ex.toString());
+        }
+        return usuarios;
+    }
+
+    public Usuario readUsuarioListado(JsonReader reader) throws IOException {
+        Usuario usuario = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            switch (name) {
+                case "Nombre":
+                    usuario = new Usuario(reader.nextString());
+                    usuario.conectar();
+                    break;
+                default:
+                    reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return usuario;
     }
 
     /**
      * Autentica a un usuario en el servidor
+     *
      * @param usuario que debe tener la información necesaria para autenticar
      * @return Devuelve el Usuario con la información del servidor
      */
-    public Usuario login(Usuario usuario){
+    public Usuario login(Usuario usuario) {
         usuario.conectar();
         UsuarioProxy.usuario = usuario;
         return usuario;
@@ -58,10 +149,11 @@ public class UsuarioProxy {
 
     /**
      * Marca en el servidor que el usuario aparecera como desconectado
+     *
      * @param usuario a desconectar
      * @return el usuario desconectado
      */
-    public Usuario logout(Usuario usuario){
+    public Usuario logout(Usuario usuario) {
         usuario.desconectar();
         UsuarioProxy.usuario.desconectar();
         return usuario;
@@ -69,12 +161,13 @@ public class UsuarioProxy {
 
     /**
      * Registra y Autentifica a un usuario en el servidor
+     *
      * @param username nombre del usuario
      * @param password contraseña del usuario
      * @return el Usuario autentificado
      */
-    public Usuario registrar(String username, String password){
-        Usuario user = new Usuario(username,password);
+    public Usuario registrar(String username, String password) {
+        Usuario user = new Usuario(username, password);
         user.conectar();
         UsuarioProxy.usuario = user;
         return user;
@@ -82,18 +175,20 @@ public class UsuarioProxy {
 
     /**
      * Trae el estado completo de un usuario
+     *
      * @param username nombre del usuario
      * @return el Usuario con todos sus datos para mostrar
      */
-    public Usuario verEstado(String username){
-        return  new Usuario(username);
+    public Usuario verEstado(String username) {
+        return new Usuario(username);
     }
 
     /**
      * Envia la foto de perfil del usuario al servidor
+     *
      * @param usuario con una foto de perfil nueva.
      */
-    public void cargarFoto(Usuario usuario){
+    public void cargarFoto(Usuario usuario) {
         UsuarioProxy.usuario = usuario;
         ///TODO: Enviar foto al server
     }
