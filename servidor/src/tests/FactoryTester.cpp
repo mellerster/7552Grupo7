f41ba79661f6091379931d3011b2086289975571
Cesar Leguizamon <fabi1816@gmail.com>
2015-05-10 01:25:54 -0300

    Se agregaron nuevos tests referidos a la nueva implementación de la factory de handlers

--- servidor/src/tests/FactoryTester.cpp
+++ servidor/src/tests/FactoryTester.cpp
@@ -1,6 +1,9 @@
 #include <string>
-#include <string.h>
 #include <memory>
+#include <unordered_map>
+#include <functional>
+
+#include <string.h>
 
 #include "include/catch.hpp"
 #include "include/hippomocks.h"
@@ -14,6 +17,7 @@
 #include "handlers/LoginRequest.hpp"
 #include "handlers/ListUsersRequest.hpp"
 #include "handlers/UserSignUpHandler.hpp"
+#include "mocks/MockedRequestHandler.hpp"
 
 
 
@@ -159,7 +163,6 @@ TEST_CASE( "Manejo de char pointers", "[punteros][params]" ){
         CHECK ( strcmp( s.c_str(), p1 ) == 0 );
     }
 
-
     SECTION ( "char array dinamico a string" ){
         const char* pSrc = "POST";
         int tam =  strlen(pSrc);
@@ -174,6 +177,97 @@ TEST_CASE( "Manejo de char pointers", "[punteros][params]" ){
         CHECK ( strcmp( s.c_str(), pDst ) == 0 );
 
         delete[] pDst;
+    }
+
+}
+
+
+TEST_CASE ( "Nueva inplemetación para la factory" ){
+    SECTION ( "Simple mapa" ){
+        std::unordered_map< std::string, std::function<int(void)> > um;
+        um["cinco"] = []{ return 5; };
+
+        int resul = 0;
+        REQUIRE_NOTHROW (
+            resul = um["cinco"]();
+        );
+
+        REQUIRE ( 5 == resul );
+    }
+
+    SECTION ( "Simple mapa con parametro" ){
+        std::unordered_map< std::string, std::function<int(int)> > um;
+        um["fun"] = [] (int a) { return a; };
+
+        int resul = 0;
+        REQUIRE_NOTHROW (
+            resul = um["fun"]( 9 );
+        );
+
+        REQUIRE ( 9 == resul );
+    }
+
+    SECTION ( "Mapa que devuelve un puntero" ){
+        std::unordered_map< int, std::function< int*(void) > > um;
+        um[1] = []{ return new int; };
+
+        int* resul = nullptr;
+        REQUIRE_NOTHROW (
+            resul = um[1]();
+        );
+
+        REQUIRE ( nullptr != resul );
+
+        delete resul;
+    }
+
+    SECTION ( "Mapa que devuelve un int y recibe una interfaz" ){
+        // Mocks
+        MockRepository mocker;
+        IDataService* mockService = mocker.Mock<IDataService>();
+        mocker.OnCall( mockService, IDataService::startSession ).Return( 999 );
+
+        std::unordered_map< int, std::function<unsigned int(IDataService&)> > um;
+        um[1] = [] (IDataService& ds) { 
+            return ds.startSession("", "");
+        };
+
+        unsigned int resul;
+        REQUIRE_NOTHROW (
+            resul = um[1](*mockService);
+        );
+
+        REQUIRE ( 999 == resul );
+    }
+
+    SECTION ( "Mapa que devuelve un puntero a una clase base" ){
+        // Arrange
+        MockRepository mocker;
+        IDataService* mockService = mocker.Mock<IDataService>();
+
+        // Act
+        std::unordered_map< int, std::function< RequestHandler*(IDataService&) > > um;
+        um[1] = [] (IDataService& ds) -> RequestHandler* { return new MockedRequestHandler(ds); };
+
+        // Assert
+        RequestHandler* rh = nullptr;
+        REQUIRE_NOTHROW (
+                rh = um[1]( *mockService );
+        );
+
+        REQUIRE ( nullptr != rh );
+
+        delete rh;
+    }
+
+    SECTION ( "Mapa que se pide un labmda inexistente" ){
+        std::unordered_map< int, std::function<int(int)> > um;
+        um[1] = [] (int i) { return i; };
+
+        auto ptrResul = um.find( 99 );
+
+        REQUIRE ( um.end() == ptrResul );
+
     }
 
 }
