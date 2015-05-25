0f386ad96b97e247c57944e910ce65e0c63fdf2d
mellerster <concdiego@gmail.com>
2015-05-25 17:16:43 -0300

    Se puede autologin nuevamente

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/MainActivity.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appmaker.mensajero;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 /**
  * Created by diego on 07/05/15.
@@ -19,39 +21,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO: Hacer login con tarea asincronica (baja prioridad)
-        /*
+
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         String usuarioGuardado= sharedPref.getString("usuario","");
 
         if(!usuarioGuardado.isEmpty()){
             Log.d("AutoLogin","Se autologeo con el usuario:" + usuarioGuardado);
             String contraseniaGuardada = sharedPref.getString("contrasenia","");
-            new UsuarioProxy().login(new Usuario(usuarioGuardado,contraseniaGuardada));
-            Intent listaUsuariosConectadosIntent = new Intent("com.example.appmaker.mensajero.ListaUsuariosConectadosActivity");
-            startActivity(listaUsuariosConectadosIntent);
-            finish();
-        }*/
+            new AutentificacionTask(usuarioGuardado,contraseniaGuardada).execute();
+        } else {
 
-        Button btnIniciarSesion = (Button)findViewById(R.id.iniciar_sesion_button);
-        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent autentificacionIntent =new Intent("com.example.appmaker.mensajero.AutentificacionActivity");
-                startActivity(autentificacionIntent );
-                finish();
-            }
-        });
+            Button btnIniciarSesion = (Button) findViewById(R.id.iniciar_sesion_button);
+            btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent autentificacionIntent = new Intent("com.example.appmaker.mensajero.AutentificacionActivity");
+                    startActivity(autentificacionIntent);
+                    finish();
+                }
+            });
 
-        Button btnRegistrarse = (Button)findViewById(R.id.crear_nueva_cuenta_button);
-        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registracionIntent =new Intent("com.example.appmaker.mensajero.RegistracionActivity");
-                startActivity(registracionIntent);
-                finish();
-            }
-        });
+            Button btnRegistrarse = (Button) findViewById(R.id.crear_nueva_cuenta_button);
+            btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent registracionIntent = new Intent("com.example.appmaker.mensajero.RegistracionActivity");
+                    startActivity(registracionIntent);
+                    finish();
+                }
+            });
+        }
     }
 
     @Override
@@ -75,5 +74,39 @@ public class MainActivity extends ActionBarActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Representa un task asincronico para la autentificacion del usuario.
+     */
+    public class AutentificacionTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String usuario;
+        private final String contrasenia;
+
+        AutentificacionTask(String usuario, String contrasenia) {
+            this.usuario = usuario;
+            this.contrasenia = contrasenia;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            Usuario usuario = null;
+            usuario = new UsuarioProxy().login(new Usuario(this.usuario, this.contrasenia));
+            return (usuario != null );
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            if (success) {
+                Intent listaUsuariosConectadosIntent = new Intent("com.example.appmaker.mensajero.ListaUsuariosConectadosActivity");
+                startActivity(listaUsuariosConectadosIntent);
+                finish();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+        }
     }
 }
