50c8c652988e17466ac6fa669a24c5967f867da7
Cesar Leguizamon <fabi1816@gmail.com>
2015-05-30 00:33:41 -0300

    Primera implementación completa de la base de datos

--- servidor/src/dataService/db/RocaDB.cpp
+++ servidor/src/dataService/db/RocaDB.cpp
@@ -1,8 +1,10 @@
 #include "RocaDB.hpp"
 
+#include "json/json.h"
 
-RocaDB::RocaDB() : m_rockdb(nullptr) {
-}
+
+HUMBLE_LOGGER( logger, "default" );
+
 
 
 RocaDB::~RocaDB() {
@@ -15,7 +17,11 @@ void RocaDB::Open(std::string fileName) {
     opt.create_if_missing = true;
 
     rocksdb::Status st = rocksdb::DB::Open( opt, fileName, &this->m_rockdb );
-    // TODO: Checkear el estado...
+
+    if (!st.ok()) {
+        HL_FATAL( logger, "Falló la apertura de la base de datos\n" + st.ToString() );
+        throw std::runtime_error( st.ToString() );
+    }
 }
 
 
@@ -28,30 +34,252 @@ void RocaDB::Close() {
 }
 
 
-bool RocaDB::ExistsUser(std::string userID);
-bool RocaDB::CreateUser(std::string userID, std::string password);
-bool RocaDB::StoreUserUbicacion(std::string userID, std::string latitud, std::string longitud);
-bool RocaDB::LoadUserUbicacion(std::string userID, std::string &latitud, std::string &longitud);
-bool RocaDB::StoreUserFoto(std::string userID, std::string foto);
-bool RocaDB::LoadUserFoto(std::string userID, std::string &foto);
+//------------------- Usuarios ----------------------------
 
-unsigned int CRocaDB::reateNewConversacion(std::vector<std::string> listaUsuarios);
-std::vector<unsigned int> RocaDB::GetMensajesConversacion(unsigned int conversacionID);
-unsigned int RocaDB::AgregarMensaje(std::string userID, unsigned int IDConversacion, std::string texto);
-std::string RocaDB::GetMensaje(unsigned int mensajeID);
 
-unsigned int RocaDB::GetIDUltimoMensaje(std::string userID, unsigned int conversacionID);
-void RocaDB::SetIDUltimoMensaje(std::string userID, unsigned int conversacionID, unsigned int mensajeID);
+bool RocaDB::ExistsUser(std::string userID) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetUserKey(userID), &val);
 
-    /*
-    cout << "Abriendo la base de datos... ";
-    rocksdb::DB* db;
-    rocksdb::Options opt;
-    opt.create_if_missing = true;
+    return !st.IsNotFound();
+}
 
-    rocksdb::Status st = rocksdb::DB::Open( opt, "testDB.bin", &db );
-    cout << st.ToString() << "!" << endl;
-    cout << endl;
 
-    delete db;
-    */
+bool RocaDB::CreateUser(std::string userID, std::string password) {
+    Json::Value jUser;
+    jUser["Password"] = password;
+
+    // Si existia algo lo sobre-escribe
+    rocksdb::Status st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetUserKey(userID), jUser.asString() );
+    return st.ok();
+}
+
+
+bool RocaDB::StoreUserUbicacion(std::string userID, std::string latitud, std::string longitud) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetUserKey(userID), &val);
+
+    if (st.IsNotFound()) {
+        return false;   // No existe el usuario
+    }
+
+    Json::Value jUser(val);
+    jUser["Latitud"] = latitud;
+    jUser["Longitud"] = longitud;
+
+    st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetUserKey(userID), jUser.asString() );
+
+    return st.ok();
+}
+
+
+bool RocaDB::LoadUserUbicacion(std::string userID, std::string &latitud, std::string &longitud) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetUserKey(userID), &val);
+
+    if (st.IsNotFound()) {
+        return false;   // No existe el usuario
+    }
+
+    Json::Value jUser(val);
+    latitud = jUser.get("Latitud", "").asString();
+    longitud = jUser.get("Longitud", "").asString();
+
+    return true;
+}
+
+
+bool RocaDB::StoreUserFoto(std::string userID, std::string foto) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetUserKey(userID), &val);
+
+    if (st.IsNotFound()) {
+        return false;   // No existe el usuario
+    }
+
+    Json::Value jUser(val);
+    jUser["Foto"] = foto;
+
+    st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetUserKey(userID), jUser.asString() );
+
+    return st.ok();
+}
+
+
+bool RocaDB::LoadUserFoto(std::string userID, std::string &foto) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetUserKey(userID), &val);
+
+    if (st.IsNotFound()) {
+        return false;   // No existe el usuario
+    }
+
+    Json::Value jUser(val);
+    foto = jUser.get("Foto", "").asString();
+
+    return true;
+}
+
+
+std::string RocaDB::GetUserKey(std::string userID) const {
+    return std::string( "U/" + userID );
+}
+
+
+//------------------------- Mensajes ----------------------------
+
+
+unsigned int RocaDB::CreateNewConversacion(std::vector<std::string> listaUsuarios) {
+    Json::Value jConv;
+    jConv["IDMensajes"] = Json::Value( Json::arrayValue );  // Array vacio
+
+    // Almacena todos los participantes de la conversacion
+    for (unsigned int i = 0; i < listaUsuarios.size(); ++i) {
+        jConv["IDUsuarios"][i] = listaUsuarios[i];
+    }
+
+    unsigned int key = GenerateNewID();
+    rocksdb::Status st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetConversationKey(key), jConv.asString() );
+
+    // Si fue exitosa la operación devuelve la clave, si no "0"
+    return st.ok() ? key : 0;
+}
+
+
+std::vector<unsigned int> RocaDB::GetMensajesConversacion(unsigned int conversacionID) {
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetConversationKey(conversacionID), &val );
+
+    if (st.IsNotFound()) {  // No existe la conversación
+        return std::vector<unsigned int>();
+    }
+
+    Json::Value jConv(val);
+
+    std::vector<unsigned int> vResul;
+    for (unsigned int i = 0; i < jConv["IDMensajes"].size(); ++i) {
+        unsigned int msgID = jConv["IDMensajes"][i].asUInt();
+
+        vResul.push_back( msgID );
+    }
+
+    return vResul;
+}
+
+
+unsigned int RocaDB::AgregarMensaje(std::string userID, unsigned int IDConversacion, std::string texto) {
+    std::string conv;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), GetConversationKey(IDConversacion), &conv );
+
+    if (st.IsNotFound()) {
+        return 0;   // La conversación no existe
+    }
+
+    unsigned int msgKey = GenerateNewID();  // Se generá una nueva clave para el mensaje
+
+    // Se generá un nuevo mensaje
+    Json::Value jMsg;
+    jMsg["IDUsuario"] = userID;
+    jMsg["Texto"] = texto;
+
+    // Se agregá el id del mensaje a la conversación
+    Json::Value jConv(conv);
+    jConv["IDMensajes"].append( msgKey );
+
+    // Se guarda el mensaje
+    st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetMessageKey(msgKey), jMsg.asString() );
+    if (!st.ok()) {
+        return 0;
+    }
+
+    // Se guarda la conversación
+    st = this->m_rockdb->Put( rocksdb::WriteOptions(), GetConversationKey(IDConversacion), jConv.asString() );
+    if (!st.ok()) {
+        return 0;
+    }
+
+    return msgKey;
+}
+
+
+std::string RocaDB::GetMensaje(unsigned int mensajeID) {
+    std::string m;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(),GetMessageKey(mensajeID), &m );
+
+    if (st.IsNotFound()) {
+        return "";   // El mensaje no existe
+    }
+
+    Json::Value jMsg(m);
+    return jMsg["Texto"].asString();
+}
+
+
+std::string RocaDB::GetConversationKey(unsigned int convID) const {
+    return std::string( "C/" + convID );
+}
+
+
+std::string RocaDB::GetMessageKey(unsigned int msgID) const {
+    return std::string( "M/" + msgID );
+}
+
+
+//------------------------ Ultimos mensajes ----------------------------- 
+
+
+unsigned int RocaDB::GetIDUltimoMensaje(std::string userID, unsigned int conversacionID) {
+    std::string umKey = GetUltimoReciboKey(userID, conversacionID);
+
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), umKey, &val );
+
+    if ( st.IsNotFound() ) {
+        return 0;
+    }
+
+    Json::Value jMsg(val);
+    return jMsg.get("IDUltimoMensaje", 0).asUInt();
+}
+
+
+void RocaDB::SetIDUltimoMensaje(std::string userID, unsigned int conversacionID, unsigned int mensajeID) {
+    Json::Value jMsg;
+    jMsg["IDUltimoMensaje"] = mensajeID;
+
+    std::string umKey = GetUltimoReciboKey(userID, conversacionID);
+
+    this->m_rockdb->Put( rocksdb::WriteOptions(), umKey, jMsg.asString() );
+}
+
+
+std::string RocaDB::GetUltimoReciboKey(std::string userID, unsigned int convID) const {
+    return std::string( "UR/" + userID + std::to_string(convID) );
+}
+
+
+//---------------------------------------------------------------------
+
+
+unsigned int RocaDB::GenerateNewID() {
+    const std::string secuenceKey = "SecuenceKeyGenerator-9000";
+
+    // Recupera el ultimo Id generado
+    std::string val;
+    rocksdb::Status st = this->m_rockdb->Get( rocksdb::ReadOptions(), secuenceKey, &val );
+
+    if ( st.IsNotFound() ) {
+        val = "1";
+    }
+
+    // En base al anterior genera uno nuevo
+    unsigned int newGeneratedID = std::stoul(val) + 1;
+
+    // Guarda el nuevo Id y lo devuelve para ser usado
+    this->m_rockdb->Put( rocksdb::WriteOptions(), secuenceKey, std::to_string(newGeneratedID) );
+
+    return newGeneratedID;
+}
+
+
+
--- servidor/src/dataService/db/RocaDB.hpp
+++ servidor/src/dataService/db/RocaDB.hpp
@@ -1,6 +1,10 @@
 #ifndef ROCA_DATABASE_H
 #define ROCA_DATABASE_H
 
+#include <stdexcept>
+#include <vector>
+#include <string>
+
 #include "IDB.hpp"
 #include "rocksdb/db.h"
 #include "humblelogging/api.h"
@@ -12,7 +16,7 @@
  * */
 class RocaDB : public IDB {
     public:
-        RocaDB();
+        RocaDB() : m_rockdb( nullptr ) { }
         virtual ~RocaDB();
 
         virtual void Open(std::string fileName);
@@ -33,9 +37,38 @@ class RocaDB : public IDB {
         virtual unsigned int GetIDUltimoMensaje(std::string userID, unsigned int conversacionID);
         virtual void SetIDUltimoMensaje(std::string userID, unsigned int conversacionID, unsigned int mensajeID);
 
+
+    protected:
+
+        /** Dado el Id de un usuario devuelve la clave para acceder a los datos correspondientes.
+         * */
+        std::string GetUserKey(std::string userID) const;
+
+
+        /** Dado el Id de una conversación devuelve la clave para acceder a los datos correspondientes.
+         * */
+        std::string GetConversationKey(unsigned int convID) const;
+
+
+        /** Dado el Id de un mensaje devuelve la clave para acceder a los datos correspondientes.
+         * */
+        std::string GetMessageKey(unsigned int msgID) const;
+
+
+        /** Genera la clave para acceder al ultimo mensaje recibido por el usuario.
+         * */
+        std::string GetUltimoReciboKey(std::string userID, unsigned int convID) const;
+
+
+        /** Genera ids unicos en todo el sistema.
+         * */
+        unsigned int GenerateNewID();
+
+
     private:
 
         rocksdb::DB* m_rockdb;  /**< La base de datos. */
+
 };
 
 
