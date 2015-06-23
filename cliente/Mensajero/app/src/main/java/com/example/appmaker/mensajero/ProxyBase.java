b66b57411eba09c48fb0755999bea00bf62055f8
mellerster <concdiego@gmail.com>
2015-06-23 13:00:52 -0300

    Documentado ProxyBase

--- cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ProxyBase.java
+++ cliente/Mensajero/app/src/main/java/com/example/appmaker/mensajero/ProxyBase.java
@@ -4,10 +4,20 @@ import android.content.SharedPreferences;
 
 /**
  * Created by diego on 12/06/15.
+ * Clase base de los proxys
  */
 public class ProxyBase {
+    /**
+     * Url base donde esta la ip o nombre del servidor
+     */
     protected String url = "http://10.0.2.2";
+    /**
+     * Puerto al cual se conecta en el servidor
+     */
     protected String puerto = "8080";
+    /**
+     * Concatenacion de la url y del puerto junto con el directorio donde esta la api
+     */
     protected String urlBase = url+":"+puerto+"/grupo7/api/";
 
     /***
